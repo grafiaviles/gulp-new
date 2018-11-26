46053b4444305ef3c925958bd72b890c070c22ba
Cesar Aviles <grafiaviles@gmail.com>
2018-11-26 17:03:45 -0300

    init buscador

--- www/js/efectos.js
+++ www/js/efectos.js
@@ -1,5 +1,5 @@
 //SELECT
-/* $('select').each(function() {
+$('select').each(function() {
     var $this = $(this),
         numberOfOptions = $(this).children('option').length;
 
@@ -44,23 +44,4 @@
         $list.hide();
     });
 
-}); */
-
-//Paralax Header
-$(document).ready(function() {
-    var moving__background = $(".header__background");
-    $(window).scroll(function() {
-        moving__background.css('margin-top', ($(window).scrollTop()) / 3); // Parallax scrolling
-        moving__background.css('opacity', 1 - ($(window).scrollTop()) / moving__background.height()); // Fading out
-    });
 });
-
-//Hide Element
-function fixStaticPageStyles() {
-
-    document.querySelector('.fb-masthead__breadcrumb').style.display = 'none';
-    document.querySelector('.huincha-navidad-content').style.display = 'none';
-    document.querySelector('.page-wrapper').style.maxWidth = '100%'
-
-}
-fixStaticPageStyles();
