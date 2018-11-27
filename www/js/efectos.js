5dd07850b08335fcf012be82272983f787970b9a
Cesar Aviles <grafiaviles@gmail.com>
2018-11-27 14:56:32 -0300

    avance buscador

--- www/js/efectos.js
+++ www/js/efectos.js
@@ -1,47 +1,55 @@
-//SELECT
-$('select').each(function() {
-    var $this = $(this),
-        numberOfOptions = $(this).children('option').length;
+var selectBtn = document.getElementsByClassName('dropdown'),
+    dropdownMenu = document.getElementsByClassName('dropdownMenu');
 
-    $this.addClass('select-hidden');
-    $this.wrap('<div class="select"></div>');
-    $this.after('<div class="select-styled"></div>');
-
-    var $styledSelect = $this.next('div.select-styled');
-    $styledSelect.text($this.children('option').eq(0).text());
-
-    var $list = $('<ul />', {
-        'class': 'select-options'
-    }).insertAfter($styledSelect);
-
-    for (var i = 0; i < numberOfOptions; i++) {
-        $('<li />', {
-            text: $this.children('option').eq(i).text(),
-            rel: $this.children('option').eq(i).val()
-        }).appendTo($list);
+for (i = 0; i < selectBtn.length; i++) {
+    selectBtn[i].onclick = function() {
+        if (this.className.indexOf('active') > -1) {
+            for (j = 0; j < selectBtn.length; j++) {
+                removeClass(selectBtn[j], 'active')
+            }
+        } else {
+            addClass(this, 'active');
+        }
+    };
+}
+for (i = 0; i < dropdownMenu.length; i++) {
+    var child = dropdownMenu[i].children;
+    for (j = 0; j < child.length; j++) {
+        child[j].onclick = function() {
+            var text = this.innerHTML;
+            this.parentNode.previousElementSibling.children[0].innerHTML = text;
+            toggleClass(this.parentNode, 'showMenu');
+        };
     }
+}
 
-    var $listItems = $list.children('li');
-
-    $styledSelect.click(function(e) {
-        e.stopPropagation();
-        $('div.select-styled.active').not(this).each(function() {
-            $(this).removeClass('active').next('ul.select-options').hide();
-        });
-        $(this).toggleClass('active').next('ul.select-options').toggle();
-    });
-
-    $listItems.click(function(e) {
-        e.stopPropagation();
-        $styledSelect.text($(this).text()).removeClass('active');
-        $this.val($(this).attr('rel'));
-        $list.hide();
-        //console.log($this.val());
-    });
-
-    $(document).click(function() {
-        $styledSelect.removeClass('active');
-        $list.hide();
-    });
-
+window.addEventListener('click', function(event) {
+    for (i = 0; i < selectBtn.length; i++) {
+        if (event.target != selectBtn[i].children[0]) {
+            removeClass(selectBtn[i], 'active');
+        }
+    }
 });
+
+function toggleClass(el, classToToggle) {
+    var classN = el.className;
+    if (classN.indexOf(classToToggle) > -1) {
+        el.className = classN.replace(" " + classToToggle, '');
+    } else {
+        el.className = classN + " " + classToToggle;
+    }
+}
+
+function addClass(el, classToToggle) {
+    var classN = el.className
+    if (classN.indexOf(classToToggle) < 1) {
+        el.className = classN + " " + classToToggle;
+    }
+}
+
+function removeClass(el, classToToggle) {
+    var classN = el.className;
+    if (classN.indexOf(classToToggle) > -1) {
+        el.className = classN.replace(" " + classToToggle, '');
+    }
+}
